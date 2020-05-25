276c8a91af30f3eb298423b4aaec9119ae2a4fee
Kevin Vanantwerpen <kevin.v1989@gmail.com>
2020-05-25 03:03:17 +0200

    'create post'-button basic styling

--- src/components/CreatePostBtn.js
+++ src/components/CreatePostBtn.js
@@ -6,13 +6,10 @@ export default class CreatePostBtn extends Component {
   render() {
     return (
       <div>
-        <Link to="/create-post">
+        <Link to="/create-post" className="create-post">
+          <span className="create-post__txt d-block">Create <br/> Post</span>
           <FaPlus
-            className="create-post-icon"
-            onClick={() => {
-              alert("hi");
-            }}
-          />
+            className="create-post__icon" /> 
         </Link>
       </div>
     );
