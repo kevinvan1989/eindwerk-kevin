570297b011b82b01ae44e9a7fec0ebede376e0cf
Kevin Vanantwerpen <kevin.v1989@gmail.com>
2020-05-31 16:05:39 +0200

    Create general button

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Button extends Component {
+  render() {
+    const { btnText, url, type, disabled } = this.props;
+    console.log(disabled)
+    if (url) {
+      return (<Link to={url}>
+        <button>{btnText}</button>
+      </Link>)
+    }else{
+      return <button type={type} disabled={disabled}>{btnText}</button>
+    }
+  }
+}
